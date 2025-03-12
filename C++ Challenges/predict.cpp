4b2dfda25b6e651d6f5f5ec83d4404d5736ea22f
Varshini C S <varshinics123@gmail.com>
2025-03-12 20:26:43 +0530

    Add files via upload

---
+++ C++ Challenges/predict.cpp
@@ -0,0 +1,50 @@
+
+#include<iostream>
+using namespace std;
+int main(){
+    int x=1;
+    while (x==1)
+    {
+        x=x-1;
+        cout<<x<<endl;//output:0
+    }
+}
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int i=1;
+//     while (i=10)
+//     {
+//         cout<<i<<endl;//output:Infinate loop of 10
+//         i++;
+//     }
+    
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int i=10;
+//     while (i=20)
+//     {
+//         cout<<"A computer buff!";//output:Infinate Loop
+//     }
+    
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int x=4,y=0;
+//     while (x>=0)
+//     {
+//         x--;
+//         y++;
+//         if(x==y)continue;
+        
+//         else
+//         cout<<x<<""<<y<<endl;
+//     }
+    
+// }
