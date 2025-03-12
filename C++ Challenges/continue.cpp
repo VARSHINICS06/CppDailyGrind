5fb9fd5ae8dd7ee5e65a8782a6bb830a5fd5396e
Varshini C S <varshinics123@gmail.com>
2025-03-12 20:25:51 +0530

    Add files via upload

---
+++ C++ Challenges/continue.cpp
@@ -0,0 +1,53 @@
+//print odd number from 1 to 100 using continue
+
+#include<iostream>
+using namespace std;
+int main(){
+    for(int i=1;i<=100;i++){
+        if(i%2==0) continue;
+        
+        cout<<i<<" ";
+    }
+
+}
+
+
+//print even number from 1 to 100 using continue
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     for(int i=1;i<=100;i++){
+//         if(i%2!=0) continue;
+        
+//         cout<<i<<" ";
+//     }
+
+// }
+
+
+
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     for(int i=1;i<=20;i++){
+//         if(i==3) continue;
+//         if(i==8) continue;
+//         cout<<i<<" ";
+//     }
+
+// }
+
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+    
+//     for(int i=1;i<=20;i++){
+//         if(i!=3 && i!=8){
+//             cout<<i<<" ";
+//         }
+//     }
+
+// }
