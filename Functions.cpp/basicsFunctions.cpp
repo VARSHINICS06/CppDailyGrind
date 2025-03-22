b0492ca251ef5765ac8759e19ed40611c0de0ffb
Varshini C S <varshinics123@gmail.com>
2025-03-22 23:26:22 +0530

    Add files via upload

---
+++ Functions.cpp/basicsFunctions.cpp
@@ -0,0 +1,31 @@
+// #include<iostream>
+// using namespace std;
+// void greet(){
+//     cout<<"Good morning"<<endl;
+//     cout<<"How are You"<<endl;
+// }
+// int main(){
+//     greet();
+//     cout<<"Hey"<<endl;
+//     greet();
+
+
+// }
+
+#include<iostream>
+using namespace std;
+void usa(){
+    cout<<"You are in USA"<<endl;
+    cout<<"How are You?"<<endl;
+}
+void India(){
+    cout<<"You are in India"<<endl;
+    cout<<"How are You?"<<endl;
+}
+int main(){
+    usa();
+    cout<<"Hey"<<endl;
+    India();
+
+
+}
