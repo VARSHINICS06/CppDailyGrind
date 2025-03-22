586bc9ab52ce0a70a84b030764f5680bfe35499c
Varshini C S <varshinics123@gmail.com>
2025-03-22 23:27:19 +0530

    Add files via upload

---
+++ Functions.cpp/libraryFunction.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    // cout << max(2, 9) ;
+    // cout << min(6, 9)<<endl;
+    // cout<<cbrt(18);
+    // cout << sqrt(4)<<endl;
+    cout << pow(3, 4);
+}
