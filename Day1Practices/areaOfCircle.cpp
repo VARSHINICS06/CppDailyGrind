2aa5c8b8b83c1a114dcc34cc09aa851ce5129074
Varshini C S <varshinics123@gmail.com>
2025-06-02 23:36:09 +0530

    Update areaOfCircle.cpp

--- Day1Practices/areaOfCircle.cpp
+++ Day1Practices/areaOfCircle.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main(){
     float r;
     float a=3.14*r*r;
-    cout<<"Enter the radius of Circle"<<endl;
+    cout<<"Enter the radius Circle"<<endl;
     cin>>r;
     cout<<"Area of the Circle : "<<a;
 
